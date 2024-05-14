fc791fd05eea3a729497c57d944680212358acd2
Claudia Gutierrez-Semprun <claudia@Claudias-MacBook-Air.local>
2024-05-14 12:08:15 +0300

    This is the version without the buttons

--- indext.js
+++ indext.js
@@ -1,5 +1,11 @@
 
-
+function getPlayerChoice () {
+  var randomPic = Math.floor(Math.random() * 3 ) + 1;
+  var randomPicImage = "Img" + randomPic + ".png";
+  var randomImageSource = "images/" + randomPicImage;
+  document.querySelector(".playerImage").setAttribute("src", randomImageSource);
+  return randomPicImage;  // Return the file name
+  };
 
 
 function getComputerChoice () {
@@ -14,29 +20,28 @@ return randomPicImage;  // Return the file name
 
 function getWinner (playerChoice, ComputerChoice) {
   if (playerChoice === ComputerChoice) {
-     alert("It's a tie");
+     return"It's a tie";
   } else if (
     (playerChoice === 'Img1.png' && ComputerChoice === 'Img3.png')||
      (playerChoice === 'Img2.png' && ComputerChoice === 'Img1.png')||
      (playerChoice === 'Img3.png' && ComputerChoice === 'Img2.png')
      ) {
-      alert("You win!");
+      return"You win!";
      } else {
-      alert("You Lose Fool!!");
+      return"You Lose Fool!!";
      }
 }
 
 
-document.getElementById('rock').addEventListener('click', ()=> game('Img1.png'));
-document.getElementById('paper').addEventListener('click', ()=> game('Img2.png'));
-document.getElementById('sciccors').addEventListener('click', ()=> game('Img3.png'));
 
 
-function game (playerChoice) {
+
+function game () {
   const ComputerChoice = getComputerChoice();
+  const playerChoice = getPlayerChoice();
   const result = getWinner (playerChoice, ComputerChoice);
-  result = h1.body.innerHTML = "Good game!! ";
+  document.querySelector('h1').innerHTML = result;
 };
 
-
+game();
 
