bd4bd64f1f7a52d291dd1c171c0aff695cc8e2ff
Claudia Gutierrez-Semprun <claudia@Claudias-MacBook-Air.local>
2024-05-15 09:05:40 +0300

    last version update images and css

--- indext.js
+++ indext.js
@@ -1,47 +1,49 @@
-
 function getPlayerChoice () {
   var randomPic = Math.floor(Math.random() * 3 ) + 1;
   var randomPicImage = "Img" + randomPic + ".png";
   var randomImageSource = "images/" + randomPicImage;
   document.querySelector(".playerImage").setAttribute("src", randomImageSource);
   return randomPicImage;  // Return the file name
-  };
-
-
-function getComputerChoice () {
-var randomPic = Math.floor(Math.random() * 3 ) + 1;
-var randomPicImage = "Img" + randomPic + ".png";
-var randomImageSource = "images/" + randomPicImage;
-document.querySelector(".comImage").setAttribute("src", randomImageSource);
-return randomPicImage;  // Return the file name
 };
 
-
+function getComputerChoice () {
+  var randomPic = Math.floor(Math.random() * 3 ) + 1;
+  var randomPicImage = "Img" + randomPic + ".png";
+  var randomImageSource = "images/" + randomPicImage;
+  document.querySelector(".comImage").setAttribute("src", randomImageSource);
+  return randomPicImage;  // Return the file name
+};
 
 function getWinner (playerChoice, ComputerChoice) {
   if (playerChoice === ComputerChoice) {
-     return"It's a tie";
+     return "It's a tie";
   } else if (
     (playerChoice === 'Img1.png' && ComputerChoice === 'Img3.png')||
      (playerChoice === 'Img2.png' && ComputerChoice === 'Img1.png')||
      (playerChoice === 'Img3.png' && ComputerChoice === 'Img2.png')
      ) {
-      return"You win!";
+      return "You win!";
      } else {
-      return"You Lose Fool!!";
+      return "You Lose Fool!!";
      }
 }
-
-
-
-
 
 function game () {
   const ComputerChoice = getComputerChoice();
   const playerChoice = getPlayerChoice();
   const result = getWinner (playerChoice, ComputerChoice);
-  document.querySelector('h1').innerHTML = result;
+
+  var resultText = document.querySelector('h1');
+
+  if (result === 'You win!') {
+    resultText.className = 'win';
+  } else if (result === 'You Lose Fool!!') {
+    resultText.className = 'lose';
+  } else {
+    resultText.className = 'tie';
+  }
+
+  resultText.innerHTML = result;
 };
 
 game();
-
